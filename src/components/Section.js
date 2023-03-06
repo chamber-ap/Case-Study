d01684beef8e219b48cb517ba8c59ee9b4ad8873
Piyush Yadav <piyush19.09yadav@gmail.com>
2023-03-06 23:40:06 +0530

    Changed the Navbar

--- src/components/Section.js
+++ src/components/Section.js
@@ -14,7 +14,7 @@ function Section() {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown" style={{width:"1066px"}}>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown" style={{width:"1320px"}}>
             <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
